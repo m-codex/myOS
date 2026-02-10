eb1a1a10406a64e8cbc06ad6acb75b243569396b
m-codex <97963158+m-codex@users.noreply.github.com>
2026-02-10 21:02:49 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+rpm-ostree install tmux
 
 # Use a COPR Example:
 #
